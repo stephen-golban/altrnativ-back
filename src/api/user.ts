7eb1ac0df7136a6f77531596183edb44aec29a99
sgolban <s.golban@staffcapital.nl>
2023-06-12 16:29:26 +0300

    news

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
+import Parser from "rss-parser";
 import { Router, Response, Request } from "express";
 
 const router = Router();
+
+const parser = new Parser();
 
 router.get("/me", async (req: any, res: Response) => {
   try {
@@ -20,13 +23,17 @@ router.get("/me", async (req: any, res: Response) => {
 
 router.get("/news", async (req: Request, res: Response) => {
   try {
-    const response = await axios.get("https://saurav.tech/NewsAPI");
-    if (!response.data) {
-      return res
-        .status(400)
-        .json({ message: "Something went wrong retrieving the news." });
-    }
-    return res.status(201).json({ data: { news: response.data } });
+    const response = await parser.parseURL(
+      "https://politepol.com/fd/dCNOKTPKFaiZ"
+    );
+    const mapped = response.items.map((item) => {
+      return {
+        title: item.title,
+        actualite_url: item.link,
+        image_uri: item.enclosure?.url,
+      };
+    });
+    return res.status(201).json({ data: { news: mapped } });
   } catch (error) {
     res.status(500).json({ error });
   }
